65bb841f965e21f367798022d620a3c7edce817e
bing-ye li <>
2020-06-03 18:04:40 +0800

    update

---
+++ index.js
@@ -0,0 +1,20 @@
+let ws = new WebSocket('ws://localhost:3000')
+
+//開啟後執行的動作，指定一個 function 會在連結 WebSocket 後執行
+ws.onopen = () => {
+    console.log('open connection')
+}
+
+//關閉後執行的動作，指定一個 function 會在連結中斷後執行
+ws.onclose = () => {
+    console.log('close connection')
+}
+
+//接收 Server 發送的訊息
+ws.onmessage = (event) => {
+  if(event.data === 'ping'){
+    console.log('pong')
+  }else{
+    console.log(event)
+  }
+}
